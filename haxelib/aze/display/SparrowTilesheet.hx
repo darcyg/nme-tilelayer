7ef68d03f279454f488c1e87d9a12e225b7062bd
JbIPS <jeanbaptiste.richardet@gmail.com>
2013-06-07 11:01:28 +0200

    Fixes

--- haxelib/aze/display/SparrowTilesheet.hx
+++ haxelib/aze/display/SparrowTilesheet.hx
@@ -6,19 +6,19 @@ import nme.geom.Rectangle;
 import nme.Lib;
 
 /**
-parrow spritesheet parser for TileLayer
- * - supports animations
- * - supports sprite trimming
- * - does NOT support sprite rotation
- * @author Philippe / http://philippe.elsass.me
- */
+* Sparrow spritesheet parser for TileLayer
+* - supports animations
+* - supports sprite trimming
+* - does NOT support sprite rotation
+* @author Philippe / http://philippe.elsass.me
+*/
 class SparrowTilesheet extends TilesheetEx
 {
 
-	public function new(img:BitmapData, xml:String, textureScale:Float = 1.0) 
+	public function new(img:BitmapData, xml:String)
 	{
-		super(img, textureScale);
-		
+		super(img);
+
 		var ins = new Point(0,0);
 		var x = new haxe.xml.Fast( Xml.parse(xml).firstElement() );
 
@@ -26,29 +26,26 @@ class SparrowTilesheet extends TilesheetEx
 		{
 			var name = texture.att.name;
 			var rect = new Rectangle(
-				Std.parseFloat(texture.att.x), Std.parseFloat(texture.att.y),
-				Std.parseFloat(texture.att.width), Std.parseFloat(texture.att.height));
-			
+			Std.parseInt(texture.att.x), Std.parseInt(texture.att.y),
+			Std.parseInt(texture.att.width), Std.parseInt(texture.att.height));
+
 			var size = if (texture.has.frameX) // trimmed
-					new Rectangle(
-						Std.parseInt(texture.att.frameX), Std.parseInt(texture.att.frameY),
-						Std.parseInt(texture.att.frameWidth), Std.parseInt(texture.att.frameHeight));
-				else 
-					new Rectangle(0, 0, rect.width, rect.height);
-			
-			//trace([name, rect.x, rect.y, rect.width, rect.height, size.x, size.y, size.width, size.height]);
-			
+							new Rectangle(
+								Std.parseInt(texture.att.frameX), Std.parseInt(texture.att.frameY),
+								Std.parseInt(texture.att.frameWidth), Std.parseInt(texture.att.frameHeight));
+						else
+							new Rectangle(0,0, rect.width, rect.height);
+
 			#if flash
-			var bmp = new BitmapData(cast size.width, cast size.height, true, 0);
-			ins.x = -size.left;
-			ins.y = -size.top;
-			bmp.copyPixels(img, rect, ins);
-			addDefinition(name, size, bmp);
+				var bmp = new BitmapData(cast size.width, cast size.height, true, 0);
+				ins.x = -size.left;
+				ins.y = -size.top;
+				bmp.copyPixels(img, rect, ins);
+				addDefinition(name, size, bmp);
 			#else
-			var center = new Point((size.x + size.width / 2), (size.y + size.height / 2));
-			addDefinition(name, size, rect, center);
+				var center = new Point(size.x + size.width / 2, size.y + size.height / 2);
+				addDefinition(name, size, rect, center);
 			#end
 		}
 	}
-
 }
--- haxelib/aze/display/TilesheetEx.hx
+++ haxelib/aze/display/TilesheetEx.hx
@@ -19,18 +19,17 @@ using StringTools;
 */
 class TilesheetEx extends Tilesheet
 {
-
 	var defs:Array<String>;
 	var sizes:Array<Rectangle>;
-	
+
 	#if haxe3
 		var anims:Map<String,Array<Int>>;
 	#else
 		var anims:Hash<Array<Int>>;
 	#end
-	
+
 	#if flash
-	var bmps:Array<BitmapData>;
+		var bmps:Array<BitmapData>;
 	#end
 
 	public function new(img:BitmapData)
@@ -38,13 +37,13 @@ class TilesheetEx extends Tilesheet
 		super(img);
 
 		defs = new Array<String>();
-		
+
 		#if haxe3
-			anims = new Map < String, Array<Int> > ();
+		anims = new Map < String, Array<Int> > ();
 		#else
-			anims = new Hash <Array<Int> > ();
+		anims = new Hash <Array<Int> > ();
 		#end
-		
+
 		sizes = new Array<Rectangle>();
 		#if flash
 		bmps = new Array<BitmapData>();
@@ -74,7 +73,7 @@ class TilesheetEx extends Tilesheet
 		var indices = new Array<Int>();
 		for (i in 0...defs.length)
 		{
-			if (defs[i].startsWith(name)) 
+			if (defs[i].startsWith(name))
 				indices.push(i);
 		}
 		anims.set(name, indices);
@@ -129,11 +128,11 @@ class TilesheetEx extends Tilesheet
 			var image = images[i];
 			img.copyPixels(image, image.rect, pos, null, null, true);
 			#if flash
-			sheet.addDefinition(names[i], image.rect, image);
+				sheet.addDefinition(names[i], image.rect, image);
 			#else
-			var rect = new Rectangle(padding, pos.y, image.width, image.height);
-			var center = new Point(image.width/2, image.height/2);
-			sheet.addDefinition(names[i], image.rect, rect, center);
+				var rect = new Rectangle(padding, pos.y, image.width, image.height);
+				var center = new Point(image.width/2, image.height/2);
+				sheet.addDefinition(names[i], image.rect, rect, center);
 			#end
 			pos.y += image.height + spacing;
 		}
@@ -146,65 +145,4 @@ class TilesheetEx extends Tilesheet
 		while (p < v) p = p << 1;
 		return p;
 	}
-}
-
-#if flash
-inline public function getBitmap(indice:Int):BitmapData
-{
-return bmps[indice];
-}
-#end
-
-static public function createFromAssets(fileNames:Array<String>, padding:Int=0, spacing:Int=0)
-{
-var names:Array<String> = [];
-var images:Array<BitmapData> = [];
-for(fileName in fileNames)
-{
-var name = fileName.split("/").pop();
-var image = nme.Assets.getBitmapData(fileName);
-names.push(name);
-images.push(image);
-}
-return createFromImages(names, images, padding, spacing);
-}
-
-static public function createFromImages(names:Array<String>, images:Array<BitmapData>, padding:Int=0, spacing:Int=0)
-{
-var width = 0;
-var height = padding;
-for(image in images)
-{
-if (image.width + padding*2 > width) width = image.width + padding*2;
-height += image.height + spacing;
-}
-height -= spacing;
-height += padding;
-
-var img = new BitmapData(closestPow2(width), closestPow2(height), true, 0);
-var sheet = new TilesheetEx(img);
-
-var pos = new Point(padding, padding);
-for(i in 0...images.length)
-{
-var image = images[i];
-img.copyPixels(image, image.rect, pos, null, null, true);
-#if flash
-sheet.addDefinition(names[i], image.rect, image);
-#else
-var rect = new Rectangle(padding, pos.y, image.width, image.height);
-var center = new Point(image.width/2, image.height/2);
-sheet.addDefinition(names[i], image.rect, rect, center);
-#end
-pos.y += image.height + spacing;
-}
-return sheet;
-}
-
-static public function closestPow2(v:Int)
-{
-var p = 2;
-while (p < v) p = p << 1;
-return p;
-}
 }
